8e856f30daeba613a7268bbd9bf5e2b9e21e769f
Paolo Dapul <paolodapultech@gmail.com>
2023-05-22 22:23:06 +0800

    Fetch threads in Threads page

--- src/app/threads/page.tsx
+++ src/app/threads/page.tsx
@@ -1,3 +1,28 @@
-export default function Threads() {
-  return <div>Threads</div>;
+import { Thread } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const threads = await prisma.thread.findMany();
+  return threads;
+}
+
+async function getThreads() {
+  const results: Thread[] = await main();
+  await prisma.$disconnect();
+  return results;
+}
+
+export default async function Threads() {
+  const threads: Thread[] = await getThreads();
+
+  return (
+    <div>
+      <h1>Threads</h1>
+      {threads.map((thread) => (
+        <h2>{thread.title}</h2>
+      ))}
+    </div>
+  );
 }
